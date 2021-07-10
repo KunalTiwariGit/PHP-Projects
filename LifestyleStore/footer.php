808b8ba688911836694be5e61da91347df168f22
KunalTiwariGit <73419172+KunalTiwariGit@users.noreply.github.com>
2021-07-10 14:15:09 +0530

    Add files via upload

---
+++ LifestyleStore/footer.php
@@ -0,0 +1,8 @@
+<footer class="footer"> 
+               <div class="container">
+               <center>
+                   <p>All Rights Reserved &copy;LifestyleStore2021 | Contact Us: +91 90000 00000</p>
+                   <p>Created by Kunal Tiwari</p>
+               </center>
+               </div>
+           </footer>
